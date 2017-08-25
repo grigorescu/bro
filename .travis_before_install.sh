80c412524af161df9d0a30de5d93f3648fec5ef6
Vlad Grigorescu <grigorescu@gmail.com>
2017-08-25 13:55:50 -0500

    Add .travis_before_install.sh script.

---
+++ .travis_before_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+install_caf(ver)
+{
+    ver=$1
+
+    wget github.com/actor-framework/actor-framework/archive/${ver}.tar.gz -O caf-${ver}.tar.gz
+    tar xzf caf-${ver}.tar.gz
+    rm caf-${ver}.tar.gz
+
+    cd actor-framework-${ver} && ./configure --prefix=/usr/local && make install
+    cd ..
+}
+
+install_caf("0.14.4")
